4e7e3d4a523dff21a8415cb2a6a1371e066ec99b
Kenny Van Hoylandt <kenny.vanhoylandt@hotmail.com>
2016-09-24 17:57:26 +0200

    added configuration files

---
+++ CreateDomain/DomainConfiguration.ps1
@@ -0,0 +1,73 @@
+﻿#requires -modules xActiveDirectory
+Configuration NewDomain {
+    Param (
+        [Parameter(Mandatory)]
+        [PSCredential]$DomainAdminCredential,
+        [Parameter(Mandatory)]
+        [PSCredential]$AdminCredential
+    )
+    Import-DscResource -ModuleName xActiveDirectory,PSDesiredStateConfiguration
+
+    Node $AllNodes.Where({$_.Role -eq 'FirstDC'}).NodeName {
+        $Node.WindowsFeatures.Foreach({
+            WindowsFeature $_ {
+                Name   = $_
+                Ensure = 'Present'
+            }
+        })
+        xADDomain $Node.DomainName {
+            DomainName                    = $Node.DomainName
+            DomainAdministratorCredential = $AdminCredential
+            SafemodeAdministratorPassword = $AdminCredential
+            DomainNetbiosName             = $Node.DomainNetbiosName
+            DependsOn                     = '[WindowsFeature]AD-Domain-Services'
+        }
+        foreach ($Department in $Node.Departments) {
+            xADOrganizationalUnit $Department {
+                Name                            = $Department
+                Path                            = $Node.DomainDistinguishedName
+                Ensure                          = 'Present'
+                Credential                      = $DomainAdminCredential
+            }
+        
+            $Node.SubOUs.Foreach({
+                xADOrganizationalUnit "$Department+$_" {
+                    Name                            = $_
+                    Path                            = "OU=$Department,$($Node.DomainDistinguishedName)"
+                    Ensure                          = 'Present'
+                    Credential                      = $DomainAdminCredential
+                }
+            })
+        }
+    }
+    Node $AllNodes.Where({$_.Role -eq 'SecondDC'}).NodeName {
+        $Node.WindowsFeatures.Foreach({
+            WindowsFeature $_ {
+                Name   = $_
+                Ensure = 'Present'
+            }
+        })
+        xWaitForADDomain FirstDC {
+            DomainName = $Node.DomainName
+            DomainUserCredential = $DomainAdminCredential
+            RetryIntervalSec = $Node.RetryIntervalSec
+            RetryCount = $Node.RetryCount
+            DependsOn = '[WindowsFeature]AD-Domain-Services'
+        }
+        xADDomainController $Node.NodeName {
+            DomainName = $Node.DomainName
+            DomainAdministratorCredential = $DomainAdminCredential
+            SafemodeAdministratorPassword = $AdminCredential
+            DependsOn = '[xWaitForADDomain]FirstDC'
+        }
+    }
+}
+
+$params = @{
+    OutputPath = 'C:\temp'
+    ConfigurationData = 'C:\temp\DomainConfigData.psd1'
+    DomainAdminCredential = (Get-Credential -Message 'DomainAdminCreds')
+    AdminCredential = (Get-Credential -Message 'LocalAdminCreds')
+}
+
+NewDomain @params
---
+++ CreateDomain/LCM.ps1
@@ -0,0 +1,25 @@
+﻿#region create .meta.mof and apply LCM config
+$cimsessionDC03 = New-CimSession -ComputerName DEMO-DC03 -Credential (Get-Credential)
+$cimsessionDC04 = New-CimSession -ComputerName DEMO-DC04 -Credential (Get-Credential)
+
+$nodes = 'DEMO-DC04','DEMO-DC03'
+
+[DscLocalConfigurationManager()]
+Configuration LCM
+{
+    Node $nodes
+    {
+        Settings
+        {
+            RefreshMode          = 'Push'
+            ConfigurationMode    = 'ApplyAndAutoCorrect'
+            ActionAfterReboot    = 'ContinueConfiguration'
+            RebootNodeIfNeeded   = $true
+        }
+    }
+}
+LCM -OutputPath 'C:\Temp'
+
+Set-DscLocalConfigurationManager -Path 'C:\temp' -CimSession $cimsessionDC03,$cimsessionDC04 -Verbose -Force
+
+#endregion
